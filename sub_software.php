3613377be91e42175ede4c6944a110c2c89d8a49
Truong Nam Kien <kien@trinetcloud.com>
2014-11-21 14:11:25 +0700

    Add Sub Software page

---
+++ sub_software.php
@@ -0,0 +1,248 @@
+<!DOCTYPE html>
+<!--[if IE 8]> <html lang="en" class="ie8"> <![endif]-->  
+<!--[if IE 9]> <html lang="en" class="ie9"> <![endif]-->  
+<!--[if !IE]><!--> <html lang="en"> <!--<![endif]-->  
+    <head>
+        <title>Unify | Welcome...</title>
+
+        <!-- Meta -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="">
+
+        <!-- Favicon -->
+        <link rel="shortcut icon" href="favicon.ico">
+
+        <!-- CSS Global Compulsory -->
+        <link rel="stylesheet" href="assets/plugins/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/css/style.css">
+
+        <!-- CSS Implementing Plugins -->
+        <link rel="stylesheet" href="assets/plugins/line-icons/line-icons.css">
+        <link rel="stylesheet" href="assets/plugins/font-awesome/css/font-awesome.min.css">
+        <link rel="stylesheet" href="assets/css/plugins/brand-buttons/brand-buttons.css">
+        <link rel="stylesheet" href="assets/css/plugins/brand-buttons/brand-buttons-inversed.css">
+        <link rel="stylesheet" href="assets/css/pages/page_one.css">
+
+        <!--[if lt IE 9]><link rel="stylesheet" href="assets/plugins/revolution-slider/rs-plugin/css/settings-ie8.css" type="text/css" media="screen"><![endif]-->
+
+        <!-- CSS Theme -->    
+        <link rel="stylesheet" href="assets/css/theme-colors/default.css">
+        <link rel="stylesheet" href="assets/css/plugins/hover-effect/css/custom-hover-effects.css">
+        <link rel="stylesheet" href="assets/css/pages/page_job_inner.css">
+
+        <!-- CSS Customization -->
+        <link rel="stylesheet" href="assets/css/custom.css">
+    </head> 
+
+    <body class="header-fixed">
+        <div class="wrapper">
+            <?php include 'pagelet/top_nav.php'; ?>
+            <?php include 'pagelet/main_menu.php'; ?>
+
+
+            <div class="breadcrumbs">
+                <div class="container">
+                    <h1 class="pull-left">Sub Software</h1>
+                    <ul class="pull-right breadcrumb">
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="software.php">Software</a></li>
+                        <li class="active">Sub Software</li>
+                    </ul>
+                </div>
+            </div>
+
+
+            <link rel="stylesheet" href="assets/css/pages/portfolio-v1.css">
+            <div class="container content">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="funny-boxes funny-boxes-colored funny-boxes-red">
+                            <div class="row">
+                                <div class="col-md-4 funny-boxes-img">
+                                    <h2><a href="#">Blue Box</a></h2>
+                                    <ul class="list-unstyled funny-boxes-rating">
+                                        <li><i class="fa fa-star"></i></li>
+                                        <li><i class="fa fa-star"></i></li>
+                                        <li><i class="fa fa-star"></i></li>
+                                        <li><i class="fa fa-star"></i></li>
+                                        <li><i class="fa fa-star-o"></i></li>
+                                    </ul>
+                                    <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum.</p>
+                                    <a href="#" class="btn-u btn-u-red">read more</a>
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="responsive-video">
+                                        <iframe src="http://player.vimeo.com/video/47911018" width="530" height="300" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+                                    </div>
+                                </div>
+                            </div>                            
+                        </div>
+                    </div>
+                </div>          
+
+                <div class="heading heading-v1 margin-bottom-40">
+                    <h2>Vimeo &amp; Youtube Videos</h2>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled.</p>
+                </div>
+
+                <div class="container content portfolio-4-columns">   
+                    <div class="row"> 
+                        <div class="col-md-3 col-sm-6">
+                            <div class="view view-tenth">
+                                <img class="img-responsive" src="assets/img/main/1.jpg" alt="">
+                                <div class="mask">
+                                    <h2>Portfolio Item</h2>
+                                    <p>At vero eos et accusamus et iusto odio dignissimos dolores et quas molestias excepturi sint occaecati cupiditate non provident.</p>
+                                    <a href="portfolio_item.html" class="info">Read More</a>
+                                </div>                
+                            </div>
+                        </div>
+                        <div class="col-md-3 col-sm-6">
+                            <div class="view view-tenth">
+                                <img class="img-responsive" src="assets/img/main/2.jpg" alt="">
+                                <div class="mask">
+                                    <h2>Portfolio Item</h2>
+                                    <p>At vero eos et accusamus et iusto odio dignissimos dolores et quas molestias excepturi sint occaecati cupiditate non provident.</p>
+                                    <a href="portfolio_item.html" class="info">Read More</a>
+                                </div>                
+                            </div>
+                        </div>
+                        <div class="col-md-3 col-sm-6">
+                            <div class="view view-tenth">
+                                <img class="img-responsive" src="assets/img/main/5.jpg" alt="">
+                                <div class="mask">
+                                    <h2>Portfolio Item</h2>
+                                    <p>At vero eos et accusamus et iusto odio dignissimos dolores et quas molestias excepturi sint occaecati cupiditate non provident.</p>
+                                    <a href="portfolio_item.html" class="info">Read More</a>
+                                </div>                
+                            </div>
+                        </div>
+                        <div class="col-md-3 col-sm-6">
+                            <div class="view view-tenth">
+                                <img class="img-responsive" src="assets/img/main/7.jpg" alt="">
+                                <div class="mask">
+                                    <h2>Portfolio Item</h2>
+                                    <p>At vero eos et accusamus et iusto odio dignissimos dolores et quas molestias excepturi sint occaecati cupiditate non provident.</p>
+                                    <a href="portfolio_item.html" class="info">Read More</a>
+                                </div>                
+                            </div>
+                        </div>
+                    </div><!--/row-->
+
+                    <div class="row"> 
+                        <div class="col-md-3 col-sm-6">
+                            <div class="view view-tenth">
+                                <img class="img-responsive" src="assets/img/main/3.jpg" alt="">
+                                <div class="mask">
+                                    <h2>Portfolio Item</h2>
+                                    <p>At vero eos et accusamus et iusto odio dignissimos dolores et quas molestias excepturi sint occaecati cupiditate non provident.</p>
+                                    <a href="portfolio_item.html" class="info">Read More</a>
+                                </div>                
+                            </div>
+                        </div>
+                        <div class="col-md-3 col-sm-6">
+                            <div class="view view-tenth">
+                                <img class="img-responsive" src="assets/img/main/4.jpg" alt="">
+                                <div class="mask">
+                                    <h2>Portfolio Item</h2>
+                                    <p>At vero eos et accusamus et iusto odio dignissimos dolores et quas molestias excepturi sint occaecati cupiditate non provident.</p>
+                                    <a href="portfolio_item.html" class="info">Read More</a>
+                                </div>                
+                            </div>
+                        </div>
+                        <div class="col-md-3 col-sm-6">
+                            <div class="view view-tenth">
+                                <img class="img-responsive" src="assets/img/main/6.jpg" alt="">
+                                <div class="mask">
+                                    <h2>Portfolio Item</h2>
+                                    <p>At vero eos et accusamus et iusto odio dignissimos dolores et quas molestias excepturi sint occaecati cupiditate non provident.</p>
+                                    <a href="portfolio_item.html" class="info">Read More</a>
+                                </div>                
+                            </div>
+                        </div>
+                        <div class="col-md-3 col-sm-6">
+                            <div class="view view-tenth">
+                                <img class="img-responsive" src="assets/img/main/8.jpg" alt="">
+                                <div class="mask">
+                                    <h2>Portfolio Item</h2>
+                                    <p>At vero eos et accusamus et iusto odio dignissimos dolores et quas molestias excepturi sint occaecati cupiditate non provident.</p>
+                                    <a href="portfolio_item.html" class="info">Read More</a>
+                                </div>                
+                            </div>
+                        </div>        
+                    </div><!--/row-->
+
+                    <div class="row"> 
+                        <div class="col-md-3 col-sm-6">
+                            <div class="view view-tenth">
+                                <img class="img-responsive" src="assets/img/main/9.jpg" alt="">
+                                <div class="mask">
+                                    <h2>Portfolio Item</h2>
+                                    <p>At vero eos et accusamus et iusto odio dignissimos dolores et quas molestias excepturi sint occaecati cupiditate non provident.</p>
+                                    <a href="portfolio_item.html" class="info">Read More</a>
+                                </div>                
+                            </div>
+                        </div>
+                        <div class="col-md-3 col-sm-6">
+                            <div class="view view-tenth">
+                                <img class="img-responsive" src="assets/img/main/10.jpg" alt="">
+                                <div class="mask">
+                                    <h2>Portfolio Item</h2>
+                                    <p>At vero eos et accusamus et iusto odio dignissimos dolores et quas molestias excepturi sint occaecati cupiditate non provident.</p>
+                                    <a href="portfolio_item.html" class="info">Read More</a>
+                                </div>                
+                            </div>
+                        </div>
+                        <div class="col-md-3 col-sm-6">
+                            <div class="view view-tenth">
+                                <img class="img-responsive" src="assets/img/main/1.jpg" alt="">
+                                <div class="mask">
+                                    <h2>Portfolio Item</h2>
+                                    <p>At vero eos et accusamus et iusto odio dignissimos dolores et quas molestias excepturi sint occaecati cupiditate non provident.</p>
+                                    <a href="portfolio_item.html" class="info">Read More</a>
+                                </div>                
+                            </div>
+                        </div>
+                        <div class="col-md-3 col-sm-6">
+                            <div class="view view-tenth">
+                                <img class="img-responsive" src="assets/img/main/2.jpg" alt="">
+                                <div class="mask">
+                                    <h2>Portfolio Item</h2>
+                                    <p>At vero eos et accusamus et iusto odio dignissimos dolores et quas molestias excepturi sint occaecati cupiditate non provident.</p>
+                                    <a href="portfolio_item.html" class="info">Read More</a>
+                                </div>                
+                            </div>
+                        </div>        
+                    </div><!--/row-->
+                </div>
+            </div>
+
+            <?php include 'pagelet/footer.php'; ?>
+        </div><!--/wrapper-->
+
+        <!-- JS Global Compulsory -->           
+        <script type="text/javascript" src="assets/plugins/jquery/jquery.min.js"></script>
+        <script type="text/javascript" src="assets/plugins/jquery/jquery-migrate.min.js"></script>
+        <script type="text/javascript" src="assets/plugins/bootstrap/js/bootstrap.min.js"></script> 
+        <!-- JS Implementing Plugins -->
+        <script type="text/javascript" src="assets/plugins/back-to-top.js"></script>
+        <!-- JS Customization -->
+        <script type="text/javascript" src="assets/js/custom.js"></script>
+        <!-- JS Page Level -->           
+        <script type="text/javascript" src="assets/js/app.js"></script>
+
+        <script type="text/javascript">
+            jQuery(document).ready(function() {
+                App.init();
+            });
+        </script>
+        <!--[if lt IE 9]>
+            <script src="assets/plugins/respond.js"></script>
+            <script src="assets/plugins/html5shiv.js"></script>
+            <script src="assets/js/plugins/placeholder-IE-fixes.js"></script>
+        <![endif]-->
+
+    </body>
+</html> 
